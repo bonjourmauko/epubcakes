99de0e4c68a614c930ebe580fcbd506ca411e788
maukoquiroga <mauko@pictorical.com>
2011-06-08 23:13:09 -0400

    yeah bitch. TODO: test (hoptoad/exceptional)

--- init.rb
+++ init.rb
@@ -1,8 +1,28 @@
+require 'fileutils'
+require 'yajl/json_gem'
+require 'storage'
+
 module Epubcakes
   class Init < Sinatra::Base
-  
-    get "/" do
-      "hello\n"
+    
+    mime_type :json, 'application/json'
+    
+    before do
+      content_type :json
+    end
+    
+    # ???
+    post "/" do
+      begin
+        epub = Epubcakes::Epub.new
+        epub.download params[:container]
+        epub.zip
+        response = epub.upload('ebooks', params[:container])
+        epub.flush
+        response.to_json
+      rescue => e
+        error 500, e.message.to_json
+      end
     end
   
   end
